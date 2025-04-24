65a542021f334cda170b0c5a90a74c46549a3d0f
Matheus Galazo <matheus.galazo@outlook.com>
2025-04-24 19:58:55 -0300

    Atualização Images

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,18 +4,17 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-      <Link href={"/"}>
+        <Link href={"/"}>
           <img 
             src="/images/logo.png"
             alt="Logo"
             width={40}
             height={40}
           />
-       </Link>
+        </Link>
         <p className="text-slate-600">Todos direitos reservados Matheus 2025.</p>
       </div>
     </footer>
   );
 };
-
 export default Footer;
