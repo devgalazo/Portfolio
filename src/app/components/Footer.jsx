48e26127a043c2616fecb178286b9c29ef75a5d1
Matheus Galazo <matheus.galazo@outlook.com>
2025-04-24 19:56:18 -0300

    Atualização

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-          <Image 
+      <Link href={"/"}>
+          <img 
             src="/images/logo.png"
             alt="Logo"
             width={40}
             height={40}
           />
+       </Link>
         <p className="text-slate-600">Todos direitos reservados Matheus 2025.</p>
       </div>
     </footer>
