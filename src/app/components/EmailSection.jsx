b8ade423f5b801ef88a4c8c5e6b49d06de7aec04
Matheus Galazo <matheus.galazo@outlook.com>
2025-04-25 14:42:19 -0300

    Correção envio de e-mail

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -10,32 +10,27 @@ const EmailSection = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     const data = {
       email: e.target.email.value,
       subject: e.target.subject.value,
       message: e.target.message.value,
     };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
+    const JSONdata = JSON.stringify(data);
+
+    const response = await fetch("/api/send", {
       method: "POST",
-      // Tell the server we're sending JSON.
       headers: {
         "Content-Type": "application/json",
       },
-      // Body of the request is the JSON data we created above.
       body: JSONdata,
-    };
+    });
 
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
+    if (response.ok) {
       setEmailSubmitted(true);
+    } else {
+      alert("Erro ao enviar email. Tente novamente.");
     }
   };
 
@@ -45,14 +40,15 @@ const EmailSection = () => {
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+
       <div className="z-10">
         <h5 className="text-lg md:text-xl font-bold text-white my-2">
-        Vamos conversar!
+          Vamos conversar!
         </h5>
         <p className="text-sm md:text-lg text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
-          
-          Estou sempre em busca de novas oportunidades e adoraria ouvir de você. Se tiver alguma dúvida ou só quiser trocar uma ideia, minha caixa de entrada está aberta. 
+          Estou sempre em busca de novas oportunidades e adoraria ouvir de você.
+          Se tiver alguma dúvida ou só quiser trocar uma ideia, minha caixa de
+          entrada está aberta.
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/devgalazo">
@@ -63,10 +59,11 @@ const EmailSection = () => {
           </Link>
         </div>
       </div>
+
       <div>
         {emailSubmitted ? (
           <p className="text-green-500 text-sm mt-2">
-            Email sent successfully!
+            Email enviado com sucesso!
           </p>
         ) : (
           <form className="flex flex-col" onSubmit={handleSubmit}>
@@ -75,7 +72,7 @@ const EmailSection = () => {
                 htmlFor="email"
                 className="text-white block mb-2 text-sm font-medium"
               >
-              Seu e-mail
+                Seu e-mail
               </label>
               <input
                 name="email"
@@ -83,9 +80,10 @@ const EmailSection = () => {
                 id="email"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="matheusgalazo@outlook.com"
+                placeholder="seu@email.com"
               />
             </div>
+
             <div className="mb-6">
               <label
                 htmlFor="subject"
@@ -99,9 +97,10 @@ const EmailSection = () => {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Somente dizer Oi'"
+                placeholder="Assunto da mensagem"
               />
             </div>
+
             <div className="mb-6">
               <label
                 htmlFor="message"
@@ -112,10 +111,12 @@ const EmailSection = () => {
               <textarea
                 name="message"
                 id="message"
+                required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="Vamos conversar sobre..."
               />
             </div>
+
             <button
               type="submit"
               className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
