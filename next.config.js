6548701178870f0c779c7dac52e0fcffd3923d41
Matheus Galazo <matheus.galazo@outlook.com>
2025-04-28 13:20:00 -0300

    Correção do  Exports Modules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,  // Garante que as URLs tenham a barra no final
+  output: 'export', 
 };
 
 module.exports = nextConfig;
