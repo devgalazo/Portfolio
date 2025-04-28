ecd4d88ab3f08af98f43d35715275970b2340135
Matheus Galazo <matheus.galazo@outlook.com>
2025-04-28 13:31:09 -0300

    Correção API

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,  // Garante que as URLs tenham a barra no final
-  output: 'export', 
+  trailingSlash: true,
+  output: 'standalone',  // ⚡ ESSENCIAL para Cloudflare Pages Functions
 };
 
 module.exports = nextConfig;
+
